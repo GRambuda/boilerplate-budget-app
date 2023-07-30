aa3ae33de9577124ccdef58193e6a9295d3d5ac2
GRambuda <103312652+GRambuda@users.noreply.github.com>
2023-07-30 23:21:51 +0200

    feat: Add get_balance method
    The get_balance method returns the current balance of the budget category based on the deposits and withdrawals that have occurred.

--- budget.py
+++ budget.py
@@ -1,6 +1,14 @@
 class Category:
 
+    def __init__(self, budget_category):
+        self.ledger = []
+        self.budget_category = budget_category
+    
+    def get_balance(self):
+        balance = 0
+        for transaction in self.ledger:
+            balance += transaction["amount"]
+        return round(balance,2)
 
 
 def create_spend_chart(categories):
-    
